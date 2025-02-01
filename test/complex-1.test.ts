92eaacc23bb102a5c2c80ea170ee1f752f71046c
ryanhs <mr.ryansilalahi@gmail.com>
2025-02-01 22:09:17 +0700

    lint

--- test/complex-1.test.ts
+++ test/complex-1.test.ts
@@ -4,13 +4,13 @@ import * as owg from '../src';
 describe('complex 1 - just 2 group', () => {
   const items: OWG_Item<number>[] = [
     { id: 1, order: 1, title: '1', parentId: null },
-    
+
     { id: 2, order: 3, title: '2', parentId: null },
     { id: 3, order: 1, title: '2.1', parentId: 2 },
     { id: 4, order: 5, title: '2.2', parentId: 2 },
 
     { id: 5, order: 5, title: '5', parentId: null },
-    
+
     { id: 6, order: 6, title: '6', parentId: null },
     { id: 7, order: 1, title: '6.1', parentId: 6 },
     { id: 8, order: 2, title: '6.2', parentId: 6 },
--- test/complex-2.test.ts
+++ test/complex-2.test.ts
@@ -4,17 +4,16 @@ import * as owg from '../src';
 describe('complex 2 - chaos order, simple swap', () => {
   const items: OWG_Item<number>[] = [
     { id: 1, order: 1, title: '1', parentId: null },
-    
+
     { id: 2, order: 3, title: '2', parentId: null },
     { id: 3, order: 1, title: '2.1', parentId: 2 },
-    
+
     // swap 4 and  5
     { id: 4, order: 4, title: '4', parentId: null },
     { id: 5, order: 3, title: '2.2', parentId: 2 },
 
-    
     { id: 6, order: 6, title: '6', parentId: null },
-    
+
     // swap 7 and 8
     { id: 7, order: 7, title: '7', parentId: null },
     { id: 8, order: 2, title: '6.1', parentId: 6 },
@@ -35,10 +34,7 @@ describe('complex 2 - chaos order, simple swap', () => {
     );
   });
 
-  it.each([[7]])(
-    'getNextItem #%d => undefined',
-    currentId => {
-      expect(owg.getNextItem(items, currentId)).toBeUndefined();
-    }
-  );
+  it.each([[7]])('getNextItem #%d => undefined', currentId => {
+    expect(owg.getNextItem(items, currentId)).toBeUndefined();
+  });
 });
